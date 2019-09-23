34ed40c72397bb0f7a357a05fc93945fbdf11620
Jake Cheng <jakejcheng@gmail.com>
2019-09-23 02:43:37 -0400

    added customized inputs

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  if(github.context.payload.pull_request.head.ref != "develop"){
-    console.log(`Pull request into master from branch develop only.`)
-    core.setFailed(`Pull request into master from branch develop only.`)
+  const targeting_branches = core.getInput('targeting-branches').split(',');
+  const targeting_type = core.getInput('targeting-type');
+  if(targeting_type != "pull_request" && targeting_type != "push"){
+    console.log(`syntax error - targeting_type`)
+    throw "targeting-type must be pull_request or push"
   }
+  var matched = false;
+  targeting_branches.forEach(function(elt){
+    if(github.context.payload[targeting_type].head.ref == elt){
+      matched = true
+    }
+  });
+
+  if(!matched){
+    console.log(`check failed`)
+    throw "targeting branch did not match any provided"
+  }
+
+  console.log(`check passed`)
 } catch (error) {
   core.setFailed(error.message);
 }
